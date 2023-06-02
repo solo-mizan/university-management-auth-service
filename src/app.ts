a771ede2616016b0b52ffd14f6f85d1bab2e1090
solo-mizan <mizanbd.eee@gmail.com>
2023-06-02 21:51:10 +0600

    src, config file added

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,4 @@ app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import config from "./config";
 import app from "./app";
 
 async function bootstarp() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/test');
+        await mongoose.connect(config.database_url as string);
         console.log(`Database is connected successfully`);
+
+        app.listen(config.port, () => {
+  console.log(`University app listening on port ${config.port}`)
+})
     }
     catch (error) {
         console.log(`Failed to connect`)
