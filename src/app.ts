56f1858886478810350a1bfa1d9d3f1dbfd76623
solo-mizan <mizanbd.eee@gmail.com>
2023-06-03 01:52:58 +0600

    ESlint, prettier, husky, lint-staged setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import { Request, Response } from 'express';
 const app: Application = express()
-const port = 3000
+const port = 3000;
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
