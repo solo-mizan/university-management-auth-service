68bb3e3eea24c1de377c2012b9c9b15217ea28ac
solo-mizan <mizanbd.eee@gmail.com>
2023-06-06 01:52:57 +0600

    user interface and model created

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import { Request, Response } from 'express';
+import express, { Application } from 'express'
+import cors from 'cors'
+import { Request, Response } from 'express'
 const app: Application = express()
-const port = 3000;
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req:Request, res:Response) => {
+// cors policy
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import config from "./config";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config'
+import app from './app'
 
 async function bootstarp() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database is connected successfully`);
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database is connected successfully`)
 
-        app.listen(config.port, () => {
-  console.log(`University app listening on port ${config.port}`)
-})
-    }
-    catch (error) {
-        console.log(`Failed to connect`)
-    }
+    app.listen(config.port, () => {
+      console.log(`University app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(`Failed to connect`)
+  }
 }
 
-bootstarp();
+bootstarp()
